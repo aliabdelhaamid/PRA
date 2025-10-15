f123520405a54ffd8b5b86353837c03f5ba8c4d9
AliAbdelhamid <aabdsal@upv.edu.es>
2025-10-15 08:09:35 +0200

    odrenat soc moet

--- PRA_2526_P1/ListArray.h
+++ PRA_2526_P1/ListArray.h
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <ostream>
 #include "List.h"
 
 template <typename T> 
@@ -6,11 +6,49 @@ class ListArray : public List<T> {
 
     private:
         // miembros privados
-	void resize(int new_size);
+	void resize(int new_size{
+		T* pepe = new pepe[new_size];
+		for(int i = 0; i < n; i++) pepe[i] = arr[i];
+		delete[] arr;
+		arr = pepe;
+		max = new_size;
+	}
+	T* arr;
+	int max;
+	int n;
+	static const int MINSIZE;
     public:
         // miembros públicos, incluidos los heredados de List<T>
-    	ListArray();
-	~ListArray();
+	void insert(int pos, T e) override{
+		if(pos > n || pos < MINSIZE ) throw std::out_of_range;
+		arr[pos] = e;
+	}    	
+	void append(T e) override{
+		arr[n + 1] = e;
+	}
+	void prepend(T e) override {
+	
+	}
+	T get(int pos) override{
+		if(pos > n || pos < MINSIZE ) throw std::out_of_range;
+		return arr[pos];
+	}
+	bool empty() override {
+		if (n = 0) return true;
+		else return false;
+	}
+	int size() override{
+		return n;
+	}
+	ListArray(){
+		T* arr = new arr[MINSIZE];	
+		MINSIZE = 2;
+	}
+	~ListArray(){
+	delete[] arr;
+	}
 	T operator[](int pos);
-	friend std::ostream& operator<<(std::ostream &out, const ListArray<T> &list);
+	friend std::iostream& operator<<(std::iostream &out, const ListArray<T> &list){
+	
+	}
 };
